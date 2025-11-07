10dee8e9cccd1b7a05c851cc72de89c4a0eeea9f
Fathi Abdelmalek <abdelmalek.fathi.2001@gmail.com>
2025-11-07 19:58:48 +0100

    update 0.3.0, add timestamp fields to password

--- passphera_shell/entities.py
+++ passphera_shell/entities.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import datetime, timezone
 
 from passphera_core.entities import Generator as GeneratorEntity
 from passphera_core.utilities import default_properties
@@ -31,6 +32,8 @@ class Password:
     text: str = field(default_factory=str)
     password: str = field(default_factory=str)
     salt: bytes = field(default_factory=lambda: bytes)
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
 
     def to_dict(self) -> dict:
         """Convert the Password entity to a dictionary."""
@@ -39,10 +42,12 @@ class Password:
             "text": self.text,
             "password": self.password,
             "salt": self.salt,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
 
     def from_dict(self, data: dict) -> None:
         """Convert a dictionary to a Password entity."""
         for key, value in data.items():
-            if key in ["context", "text", "password", "salt"]:
+            if key in ["context", "text", "password", "salt", "created_at", "updated_at"]:
                 setattr(self, key, value)
